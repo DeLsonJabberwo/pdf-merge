fd274b1d39302615710fbffc8f06e44bbceae50e
DeLsonJabberwo <pdhl2006@gmail.com>
2025-12-27 15:10:45 -0600

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+
+	pdf "github.com/pdfcpu/pdfcpu/pkg/api"
+)
+
+func main() {
+	outputFileName := flag.String("o", "output.pdf", "output file name")
+
+	flag.Parse()
+
+	inputFileNames := flag.Args()
+
+	if len(inputFileNames) == 0 {
+		dir, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("Error: %s\n", err)
+		}
+
+		var pdfs []string
+		filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if filepath.Ext(d.Name()) == ".pdf" {
+				pdfs = append(pdfs, d.Name())
+			}
+			return nil
+		})
+		inputFileNames = pdfs
+	}
+
+	err := pdf.MergeCreateFile(inputFileNames, *outputFileName, false, nil)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	}
+}
